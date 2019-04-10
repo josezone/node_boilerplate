59f44bec283e7e18a715439164863bf819ef5b9f
joset <joset@qburst.com>
2019-04-10 20:44:45 +0530

    update error

--- src/utility/errorHandler.js
+++ src/utility/errorHandler.js
@@ -1,3 +1,13 @@
+const errorHandler = (e) => {
+stack = e.stack
+  .replace(/^Error: /, '')
+  .replace(msg + '\n', '')
+  .replace(/^ +/gm, '')
+  .replace(/^at /gm, '')
+  .replace(/(?: \(|@)http.+\/([^/)]+)\)?(?:\n|$)/gm, '@$1\n')
+  .replace(/ *\(eval code(:\d+:\d+)\)(?:\n|$)/gm, '@???$1\n')
+}
+
 export class errorHandler extends Error {
   constructor(str) {
     super(str);
