cfa0d7404392132b7e30e772e22d72bf95e3ab99
josezone <jos.031@live.co.uk>
2019-04-15 11:25:44 +0530

    changes in elastic

--- src/config/elasticSearch.js
+++ src/config/elasticSearch.js
@@ -3,19 +3,14 @@ import config from "./config";
 
 const client = new elasticSearch.Client({ node: config.ELASTIC_SEARCH_HOST });
 
-client.indices.create({ index: "nodeLog" });
+client.indices.create({ index: "nodeErrorLog" });
 
-// const elasticLog = (err, req) => {
-//   client.index(
-//     {
-//       index: "nodeLog",
-//       id: "1",
-//       type: "posts",
-//       body: {
-//         PostName: "Integrating Elasticsearch Into Your Node.js Application",
-//         PostType: "Tutorial",
-//         PostBody:
-//           "This is the text of our tutorial about using Elasticsearch in your Node.js application."
-//       }
-//     });
-// };
+const elasticLog = body => {
+  client.index({
+    index: "nodeErrorLog",
+    type: "posts",
+    body
+  });
+};
+
+export default elasticLog;
