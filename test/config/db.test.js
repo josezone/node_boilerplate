3ed805907ed9b14d1b6ad36aa1396e0d4468d8e0
josezone <jos.031@live.co.uk>
2019-05-02 08:27:20 +0530

    update test

--- test/config/db.test.js
+++ test/config/db.test.js
@@ -3,3 +3,7 @@ import db from "../../src/config/db";
 test("no connectin db", () => {
   expect(db.connection()).toEqual(false);
 });
+
+test("connecting db", async() => {
+  expect(await db.init()).toBe(db);
+});
